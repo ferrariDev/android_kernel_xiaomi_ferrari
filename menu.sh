8bde824b7095a5c9deea829c26b6dd050a5dabc8
Haikal Izzuddin <webhaikal@gmail.com>
2015-12-14 15:15:45 +0800

    build: Version 2.0.5

--- menu.sh
+++ menu.sh
@@ -22,7 +22,7 @@ nocol='\033[0m'
 # Device varibles (Modify this)
 device='Xiaomi Mi4i (FERRARI)' # Device Id
 sensei_base_version='Sensei' # Kernel Id
-version='2.0.4-dev' # Kernel Version
+version='2.0.5-dev' # Kernel Version
 TC=''
 
 # Modify the following variable if you want to build
@@ -32,7 +32,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="Haikal Izzuddin"
 export KBUILD_BUILD_HOST="haikalizz"
-STRIP="~/Development/Toolchains/ubertc6.0/bin/aarch64-strip"
+STRIP="~/Development/Toolchains/ubertc6.0/bin/aarch64-linux-android-strip"
 MODULES_DIR=$KERNEL_DIR/../SenseiOutput
  
 # ----------------------------------
